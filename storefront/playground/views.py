5605e581e986d2cc214e54496dc445c69c3e3312
Zarna Pathak <pathakzarna0128@gmail.com>
2024-05-31 08:55:36 +0530

    playground/templates/hello.html
    render html page and return data dynamically.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def say_hello(request):
-    return HttpResponse("Hello World..!")
+    context = {
+        'name' : 'Zarna',
+        'age' : '21',
+        'city' : 'Ahmedabad'
+    }
+    return render(request, 'hello.html', context)
